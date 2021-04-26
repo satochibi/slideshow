e10215395a6d107ac112f0a2a4e12fc91c0fd2ec
satochibi <satochibi0120@gmail.com>
2021-04-26 18:52:41 +0900

    ボタンをなくして、時間をゆっくりに

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,44 +6,56 @@ let slideshow = {
             <div v-for="(anImage, index) in frontImages" v-bind:key="anImage.src" v-bind:id="'img'+(index+1)" class="front-image" v-show="anImage.isActive" v-bind:style="{backgroundImage: 'url(' + anImage.src + ')'}">
             </div>
         </transition-group>
-        <button v-on:click="changeImage">画像{{ imageIndex + 1 }}を表示</button>
     </div>`,
     data: function () {
         return {
             imageIndex: 0,
             isLoop: true,
             frontImages: [
-                { src: "./img/s1.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s2.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s3.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s4.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s5.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s6.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s7.png", isActive: false, timerObj: null, lifeTime: 1000 },
-                { src: "./img/s8.png", isActive: false, timerObj: null, lifeTime: 1000 }
+                { src: "./img/s1.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
+                { src: "./img/s2.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
+                { src: "./img/s3.png", isActive: false, timerObj: null, lifeTime: 1000 * 3 },
+                { src: "./img/s4.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
+                { src: "./img/s5.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
+                { src: "./img/s6.png", isActive: false, timerObj: null, lifeTime: 1000 * 12 },
+                { src: "./img/s7.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
+                { src: "./img/s8.png", isActive: false, timerObj: null, lifeTime: 1000 * 9 }
             ]
         };
     },
     methods: {
         changeImage: function () {
+            this.imageIndex = this.getRandom(0, this.frontImages.length) % this.frontImages.length;
+
             let that = this.frontImages[this.imageIndex];
             that.isActive = true;
-            that.timerObj = setTimeout(function(){
+            that.timerObj = setTimeout(function () {
                 that.isActive = false;
             }, that.lifeTime);
 
-            this.imageIndex = (this.imageIndex + 1) % this.frontImages.length;
+        },
+        getRandom: function (min, max) {
+            let random = Math.floor(Math.random() * (max + 1 - min)) + min;
+            return random;
         }
     },
     created: function () {
         let that = this;
-        this.timer = setInterval(function () {
-            // console.log("発火");
-            that.changeImage();
-        }, 1000);
+        function loop() {
+            let rand = that.getRandom(0, 1000*6);
+            setTimeout(function() {
+                that.changeImage();
+                    loop();  
+            }, rand);
+        }
+        loop();
+
+        // this.timer = setInterval(function () {
+        //     that.changeImage();
+        // }, 1000);
     },
     beforeDestroy: function () {
-        for(frontImage in this.frontImages){
+        for (frontImage in this.frontImages) {
             clearTimeout(frontImage.timerObj)
         }
         clearInterval(this.timer);
