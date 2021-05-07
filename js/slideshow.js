2ddf421190e23c4ba9a0c6b9d0fafbaf9f70a86b
satochibi <satochibi0120@gmail.com>
2021-05-07 12:11:34 +0900

    ループ付きで、CSSバージョンと同じ挙動にした

--- js/slideshow.js
+++ js/slideshow.js
@@ -9,56 +9,81 @@ let slideshow = {
     </div>`,
     data: function () {
         return {
-            imageIndex: 0,
+            time: 0,
+            animTime: 30,//sec
             isLoop: true,
             frontImages: [
-                { src: "./img/s1.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
-                { src: "./img/s2.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
-                { src: "./img/s3.png", isActive: false, timerObj: null, lifeTime: 1000 * 3 },
-                { src: "./img/s4.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
-                { src: "./img/s5.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
-                { src: "./img/s6.png", isActive: false, timerObj: null, lifeTime: 1000 * 12 },
-                { src: "./img/s7.png", isActive: false, timerObj: null, lifeTime: 1000 * 6 },
-                { src: "./img/s8.png", isActive: false, timerObj: null, lifeTime: 1000 * 9 }
+                { src: "./img/s1.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "0%", lifeTime: "30%" },
+                { src: "./img/s2.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "60%", lifeTime: "30%" },
+                { src: "./img/s3.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "70%", lifeTime: "20%" },
+                { src: "./img/s4.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "10%", lifeTime: "30%" },
+                { src: "./img/s5.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "20%", lifeTime: "30%" },
+                { src: "./img/s6.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "40%", lifeTime: "50%" },
+                { src: "./img/s7.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "30%", lifeTime: "30%" },
+                { src: "./img/s8.png", isActive: false, timerStartObj: null, timerLifeObj: null, startTime: "50%", lifeTime: "40%" }
             ]
         };
     },
     methods: {
-        changeImage: function () {
-            this.imageIndex = this.getRandom(0, this.frontImages.length) % this.frontImages.length;
-
-            let that = this.frontImages[this.imageIndex];
-            that.isActive = true;
-            that.timerObj = setTimeout(function () {
-                that.isActive = false;
-            }, that.lifeTime);
+        startClock: function(){
+            for(let index = 0; index < this.frontImages.length; index++){
+                let frontImage = this.frontImages[index];
+                let that = this;
+                frontImage.timerStartObj = setTimeout(function(){
+                    that.fireImage(index);
+                }, that.getPercent2MilliSec(frontImage.startTime));
+            }
+        },
+        fireImage: function (imageIndex) {
+            let that = this;
+            let thisImage = that.frontImages[imageIndex];
+            thisImage.isActive = true;
+            thisImage.timerLifeObj = setTimeout(function () {
+                thisImage.isActive = false;
+            }, that.getPercent2MilliSec(thisImage.lifeTime));
 
         },
         getRandom: function (min, max) {
             let random = Math.floor(Math.random() * (max + 1 - min)) + min;
             return random;
+        },
+        getPercent2Num: function (str) {
+            return Number(str.replace(/\s+/g, "").replace("%", ""));
+        },
+        getPercent2MilliSec: function (str) {
+            let animTimeMilliSec = this.animTime * 1000;
+            let ratio = this.getPercent2Num(str) / 100;
+            return animTimeMilliSec * ratio;
         }
     },
     created: function () {
         let that = this;
-        function loop() {
-            let rand = that.getRandom(0, 1000*6);
-            setTimeout(function() {
-                that.changeImage();
-                    loop();  
-            }, rand);
-        }
-        loop();
 
-        // this.timer = setInterval(function () {
-        //     that.changeImage();
-        // }, 1000);
+        // function loop() {
+        //     let rand = that.getRandom(0, 1000*6);
+        //     setTimeout(function() {
+        //         that.fireImage();
+        //             loop();  
+        //     }, rand);
+        // }
+        // loop();
+
+        if (this.isLoop) {
+            this.animLoopTimer = setInterval(function () {
+                that.startClock();
+            }, 1000 * this.animTime);
+        }
+        that.startClock();
+
     },
     beforeDestroy: function () {
         for (frontImage in this.frontImages) {
-            clearTimeout(frontImage.timerObj)
+            clearTimeout(frontImage.timerLifeObj);
+            clearTimeout(frontImage.timerStartObj);
         }
-        clearInterval(this.timer);
+        if (this.isLoop) {
+            clearInterval(this.animLoopTimer);
+        }
     }
 
 }
